2f2c1f2a3e85dec0633cf4c3f55704b6af9a47d8
reyna <reyna@DESKTOP-JHKMT70>
2018-06-30 18:27:19 -0300

    Eliminan espacios

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Persona.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Persona.java
@@ -7,13 +7,11 @@ public class Persona {
 	private  String email;
 	private Boolean esMasculino;
 	
-	
 	// contructores
 
 	public Persona() {
 		super();
 	}
-	
 	
 	public Persona(String nom, String ape, int edad, String email, Boolean esMasculino) {
 		super();
@@ -24,37 +22,29 @@ public class Persona {
 		this.esMasculino = esMasculino;
 	}
 
-
 	public String getNom() {
 		return nom;
 	}
-
 
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
-
 	public String getApe() {
 		return ape;
 	}
-
 
 	public void setApe(String ape) {
 		this.ape = ape;
 	}
 
-
 	public int getEdad() {
 		return edad;
 	}
 
-
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
-	
-
 
 	public String getEmail() {
 		return email;
@@ -65,23 +55,18 @@ public class Persona {
 		this.email = email;
 	}
 
-
 	public Boolean getEsMasculino() {
 		return esMasculino;
 	}
-
 
 	public void setEsMasculino(Boolean esMasculino) {
 		this.esMasculino = esMasculino;
 	}
 
-
 	public int sumaEdad() {
 		return this.edad+1;
 		
 	}
-
-
 	
 	public static void testPersona() {
 		Persona p1= new Persona("Rober", "ape", 21, "",true);
