0d9b4de057c625909474e9e74f7e681e0ad9516c
reyna <reyna@DESKTOP-JHKMT70>
2018-06-30 17:00:58 -0300

    add Persona

---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Persona.java
@@ -0,0 +1,28 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+public class Persona {
+	private String  nom;
+	private String  ape;
+	private int edad;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getApe() {
+		return ape;
+	}
+	public void setApe(String ape) {
+		this.ape = ape;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+}
