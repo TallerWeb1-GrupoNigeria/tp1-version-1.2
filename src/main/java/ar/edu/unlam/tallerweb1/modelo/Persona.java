5d1b0f34a5225e091ca6f6614ec719cfc376b4b1
reyna <reyna@DESKTOP-JHKMT70>
2018-06-30 19:05:18 -0300

    Elimina un atributo

---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Persona.java
@@ -0,0 +1,72 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+
+
+public class Persona {
+	
+		private String  nom;
+		private String  ape;
+		private int edad;
+		private  String email;
+		
+		// contructores
+
+		public Persona() {
+			super();
+		}
+		
+		public Persona(String nom, String ape, int edad, String email, Boolean esMasculino) {
+			super();
+			this.nom = nom;
+			this.ape = ape;
+			this.edad = edad;
+			this.email = email;
+		}
+
+		public String getNom() {
+			return nom;
+		}
+
+		public void setNom(String nom) {
+			this.nom = nom;
+		}
+
+		public String getApe() {
+			return ape;
+		}
+
+		public void setApe(String ape) {
+			this.ape = ape;
+		}
+
+		public int getEdad() {
+			return edad;
+		}
+
+		public void setEdad(int edad) {
+			this.edad = edad;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public int sumaEdad() {
+			return this.edad+1;
+			
+		}
+		
+		public static void testPersona() {
+			Persona p1= new Persona("Rober", "ape", 21, "",true);
+			System.out.println(p1.getApe() +", "+ p1.getNom());
+			System.out.println("Hola mundo.");
+		}
+	}
+	
+	
+	
